b6f795ffb05da1bb9e4c3c0fd403e4746bba0d94
jeymel9718 <jey9608@hotmail.com>
2017-10-11 03:21:55 -0600

    Update persona

--- Proyecto/Rest/Proyecto1/Proyecto1/Services/PersonaService.cs
+++ Proyecto/Rest/Proyecto1/Proyecto1/Services/PersonaService.cs
@@ -51,8 +51,17 @@ namespace Proyecto1.Services
             System.Data.SqlClient.SqlConnection conn;
             SqlCommand command;
 
-            conn = new SqlConnection("Data Source=(local);Initial Catalog=Proyecto1;Integrated Security=True");
+            conn = new SqlConnection("Data Source=MELENDEZ-JEISON\\SQLEXPRESS;Initial Catalog=Proyecto1;Integrated Security=True");
             conn.Open();
+            String comm = "Update Persona SET Contraseña=\'" + persona.Contraseña + "\',Telefono=\'" + persona.Telefono +
+                "\',DescripcionDireccion=\'" + persona.DescripcionDireccion + "\',Provincia=\'"+persona.Provincia+"\'" +
+                ",Canton=\'"+persona.Canton+"\',Distrito=\'"+persona.Distrito+"\' WHERE IdCedula=" + persona.IdCedula;
+            
+
+            command = new SqlCommand(comm,conn);
+
+            command.ExecuteNonQuery();
+            conn.Close();
 
 
 

d5efee7ad05c4fbe281a13ab6ed56059bdf7ebb1
luisf1997cr <luismuelino@gmail.com>
2017-10-17 11:55:50 -0600

    Estadistica

---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Controllers/EstadisticaController.cs
@@ -0,0 +1,41 @@
+﻿using Proyecto1.Classes;
+using Proyecto1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Proyecto1.Controllers
+{
+    public class EstadisticaController : ApiController
+    {
+        [RoutePrefix("api/Estadistica")]
+        public class EnfermedadController : ApiController
+        {
+            [HttpGet]
+            [Route("GetAllProductosVendidos")]
+            public IHttpActionResult GetAllProductosVendidos()
+            {
+                EstadisticaService con = new EstadisticaService();
+                return Ok(con.GetAllProductosVendidos());
+            }
+            [HttpGet]
+            [Route("GetAllProductosVendidosxCompania")]
+            public IHttpActionResult GetAllProductosVendidosxCompania(int id)
+            {
+                EstadisticaService con = new EstadisticaService();
+                return Ok(con.GetAllProductosVendidosxCompania(id));
+            }
+
+            [HttpGet]
+            [Route("GetAllEmpresasxVentas")]
+            public IHttpActionResult GetAllEmpresasxVentas()
+            {
+                EstadisticaService con = new EstadisticaService();
+                return Ok(con.GetAllEmpresasxVentas());
+            }
+        }
+    }
+}
---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Scripts - Views/estadistica.js
@@ -0,0 +1,32 @@
+﻿var estadistica = angular.module('Estadistica', []);
+
+estadistica.controller("AllController", function ($scope, $http, $location) {
+
+    $http.get("http://localhost:64698/api/Empresa/GetIdEmpresa?id=" + window.localStorage.getItem("idSucursal"))
+        .then(function (response) {
+            $scope.EmpresaId = response.data;
+            console.log($scope.cantidadesValue);
+            $http.get("http://localhost:64698/api/Estadistica/GetAllProductosVendidosxCompania?id=" + $scope.EmpresaId)
+                .then(function (response) {
+                    $scope.cantidadesValue = response.data;
+                    console.log($scope.cantidadesValue);
+                });
+        });
+
+
+    $http.get("http://localhost:64698/api/Estadistica/GetAllProductosVendidos")
+        .then(function (response) {
+            $scope.AllValue = response.data;
+            console.log($scope.cantidadesValue);
+        });
+
+    $http.get("http://localhost:64698/api/Estadistica/GetAllEmpresasxVentas")
+        .then(function (response) {
+            $scope.ventasValue = response.data;
+            console.log($scope.ventasValue);
+        });
+
+
+
+
+});
